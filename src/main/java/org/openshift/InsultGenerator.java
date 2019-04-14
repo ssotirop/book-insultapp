5ae9fc34ca551104a9d82b438603dea2e15ab12c
ssotirop <48923568+ssotirop@users.noreply.github.com>
2019-04-14 20:05:02 +0100

    Update InsultGenerator.java
    Code modified to include better error handling using SQLException

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -3,38 +3,55 @@ package org.openshift;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class InsultGenerator {
-	public String generateInsult() {
-		String vowels = "AEIOU";
-		String article = "an";
-		String theInsult = "";
+    public static String generateInsult() {
+        String vowels = "AEIOU";
+        String article = "an";
+        String theInsult = "";
+        String hostanme = "postgresql";
+        String port = ":5432";
+        String driverJDBC = "org.postgresql.Driver";
+        Connection connection = null;
 
-		try {
-			String databaseURL = "jdbc:postgresql://";
-			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
-			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-			String username = System.getenv("POSTGRESQL_USER");
-			String password = System.getenv("PGPASSWORD");
-			Connection connection = DriverManager.getConnection(databaseURL, username, password);
-			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
-				Statement stmt = connection.createStatement();
-				ResultSet rs = stmt.executeQuery(SQL);
-				while (rs.next()) {
-					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
-						article = "a";
-					}
-					theInsult = String.format("Thou art %s %s %s %s!", article,
-					rs.getString("first"), rs.getString("second"), rs.getString("noun"));
-				}
-				rs.close();
-				connection.close();
-			}
-		} catch (Exception e) {
-		return "Database connection problem!";
-	}
-	return theInsult;
-	}
-}
+        String username = System.getenv("POSTGRESQL_USER");
+        String password = System.getenv("PGPASSWORD");
+        String databaseURL = "jdbc:postgresql://";
+        databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+        databaseURL += port;
+        databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+
+        try {
+            Class.forName(driverJDBC);
+            connection = DriverManager.getConnection(databaseURL, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.getStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1;";
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery(SQL);
+                while (rs.next()) {
+                    if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+                        article = "a";
+                    }
+                    theInsult = String.format("Thou art %s %s %s %s!", article,
+                    rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+                }
+                rs.close();
+                connection.close();
+            } else {
+                return "Database connection problem!";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return theInsult;
+    }
